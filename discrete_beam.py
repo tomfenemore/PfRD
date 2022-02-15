239164a8a5f41ba28d6597cbd458de6240fa11ac
tomfenemore <tomfenemore@hotmail.co.uk>
2022-02-15 16:57:17 +0000

    updated GA and loading types for errors

--- discrete_beam.py
+++ discrete_beam.py
@@ -5,10 +5,12 @@ import numpy as np
 def beam(X):
     bkl_psn = X[0]
     E_root = X[1]
-    str = X[2]
+    str = 'uniform'
     twist = np.zeros(1001)
     M = np.zeros(1002)
     twist_profile = np.zeros(1001)
+    l_profile = np.zeros(1001)
+    m_profile = np.zeros(1001)
     tottw = 0
 
     for x in reversed(range(0, 1001)):
@@ -19,8 +21,17 @@ def beam(X):
 
     for x in range(1,1001):
         twist_profile[x] = twist_profile[x-1]+twist[x]
-        m_profile = twist_profile[x] * x
-    d_L = twist_profile.sum()
+        if str == 'tip':
+            l_profile[x] = twist_profile[x]
+            m_profile[x] = twist_profile[x] * x
+        elif str == 'uniform':
+            l_profile[x] = twist_profile[x]
+            m_profile[x] = twist_profile[x] * x
+        elif str == 'linear':
+            l_profile[x] = twist_profile[x] * x
+            m_profile[x] = twist_profile[x] * x * x
+
+    d_L = l_profile.sum()
     d_M = m_profile.sum()
     dM_dL = -d_M / d_L
     print(tottw)
