d325db6593ea8a32d29e0560b039cd6f99e7d2b6
Thomas Fenemore <tf17417@bristol.ac.uk>
2022-06-22 15:04:27 +0100

    FEscript working init

--- FEScript.py
+++ FEScript.py
@@ -1,5 +1,5 @@
 from abaqus import *
-from abaqusCostants import *
+#from abaqusCostants import *
 from caeModules import *
 from driverUtils import executeOnCaeStartup
 executeOnCaeStartup()
@@ -7,7 +7,6 @@ from odbAccess import *
 
 
 F=100
-openMdb(pathName='C:\Users\tf17417\OneDrive - University of Bristol\RP4FE')  # open abaqus project
-mdb.models['halfbuckle'].loads['L-100'].setValues(CF1=0,CF2=F, CF3=0)  # change a single load, needs loop for all 10
+openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')  # open abaqus project
+mdb.models['halfbuckle'].loads['L-100'].setValues(cf2=F)  # change a single load, needs loop for all 10
 # loadng points
-
