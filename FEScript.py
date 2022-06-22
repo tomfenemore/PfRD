213a6fa564b523bc74bef718a571cd260a903009
tomfenemore <tomfenemore@hotmail.co.uk>
2022-06-22 15:29:08 +0100

    run job in FE

--- FEScript.py
+++ FEScript.py
@@ -5,10 +5,16 @@ from driverUtils import executeOnCaeStartup
 executeOnCaeStartup()
 from odbAccess import *
 
-
+# Apply forcing to structure
 F=141.4
 openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')  # open abaqus project
 for i in range(10):
     string = 'L-%s00'%(i+1)
-    mdb.models['halfbuckle'].loads[string].setValues(cf2=F)  # change a single load, needs loop for all 10
-# loadng points
+    mdb.models['halfbuckle'].loads[string].setValues(cf2=F)
+
+#  Create and Run the Job
+mdb.jobs['half'].submit(consistencyChecking=OFF)
+mdb.jobs['half'].waitForCompletion()
+
+#  Get the required displacement data from the job
+ff = session.openOdb(name='half.odb')
