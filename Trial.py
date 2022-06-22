91c515462e68ab68a73809be950be0a83444c6b8
Thomas Fenemore <tf17417@bristol.ac.uk>
2022-06-22 16:43:10 +0100

    History Output Saving'

--- Trial.py
+++ Trial.py
@@ -6,18 +6,8 @@ executeOnCaeStartup()
 from odbAccess import *
 import numpy as np
 
-# Apply forcing to structure
-F=141.4
-openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')  # open abaqus project
-for i in range(10):
-    string = 'L-%s00'%(i+1)
-    mdb.models['halfbuckle'].loads[string].setValues(cf2=F)
+openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')
 
-#  Create and Run the Job
-mdb.jobs['half'].submit(consistencyChecking=OFF)
-mdb.jobs['half'].waitForCompletion()
-
-#  Get the required displacement data from the job
 f = session.openOdb(name='half.odb')
 u = np.array(f.steps['nlan'].historyRegions['Node PART-1-1.13573'].historyOutputs['UR3'].data)
 
