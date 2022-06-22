362d6d52da78f758b371c3265645f9520bdbc685
Thomas Fenemore <tf17417@bristol.ac.uk>
2022-06-22 15:19:02 +0100

    For loop on loads

--- FEScript.py
+++ FEScript.py
@@ -6,7 +6,9 @@ executeOnCaeStartup()
 from odbAccess import *
 
 
-F=100
+F=141.4
 openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')  # open abaqus project
-mdb.models['halfbuckle'].loads['L-100'].setValues(cf2=F)  # change a single load, needs loop for all 10
+for i in range(10):
+    string = 'L-%s00'%(i+1)
+    mdb.models['halfbuckle'].loads[string].setValues(cf2=F)  # change a single load, needs loop for all 10
 # loadng points
