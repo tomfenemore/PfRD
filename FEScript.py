674c1f57959de872c87667456005fe2a2ec0d312
tomfenemore <tomfenemore@hotmail.co.uk>
2022-06-23 12:08:14 +0100

    added force smear and initial force calc

--- FEScript.py
+++ FEScript.py
@@ -5,13 +5,21 @@ from driverUtils import executeOnCaeStartup
 executeOnCaeStartup()
 from odbAccess import *
 import numpy as np
+import Forcing as f
+import Force_Discretisation as FD
 
-# Apply forcing to structure
-F=141.4
+#  Define the buckle location and extent
+X = [500, 2]  # Location, Extent
+
+#  Calculate initial force by inputting X into forces class
+f_ini = f.forces(twist_profile[0], bkl_psn, E_root)
+F = FD.smear(f_ini)
+
+
 openMdb(pathName='C:\Users\\tf17417\OneDrive - University of Bristol\RP4FE\FEA\Runkel half-2018.cae')  # open abaqus project
 for i in range(10):
     string = 'L-%s00'%(i+1)
-    mdb.models['halfbuckle'].loads[string].setValues(cf2=F)
+    mdb.models['halfbuckle'].loads[string].setValues(cf2=F[0])
 
 #  Create and Run the Job
 mdb.jobs['half'].submit(consistencyChecking=OFF)
