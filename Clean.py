c6b7b4ff7c5752d6830823f4917270e4c546557e
tomfenemore <tomfenemore@hotmail.co.uk>
2022-07-05 11:48:36 +0100

    data clean process added

--- Clean.py
+++ Clean.py
@@ -1,33 +1,35 @@
 import pandas as pd
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-from sklearn import linear_model
+
 
 def clean(str):
 
-    half = pd.read_excel('/Users/tomfenemore/Downloads/FEAResults.xlsx', f'{str}')
+    half = pd.read_csv(f'U_{str}.csv')
     trim_hf = half
-    trans = half[half['LPF'].gt(0.5)].index[0]
+    trans = half[half['# LPF'].gt(0.5)].index[0]
+    #print(trans, half['# LPF'].iloc[trans])
     trim_hf = trim_hf.drop(range(trans,len(half)))
     new_hf = pd.DataFrame()
 
     for col in trim_hf:
-        if col == 'LPF':
+        if col == '# LPF':
             new_hf[col] = half[col]
         else:
-            regr = linear_model.LinearRegression()
-            x = trim_hf['LPF']
-            print(x)
-            y = trim_hf[col]
-            print(y)
+            x = trim_hf['# LPF']
+            #print(x)
+            y = trim_hf[' UR']
+            #print(y)
             m, c = np.polyfit(x, y, 1)
-            new_hf[col] = half[col] - (m * (half['LPF']) + c)
 
-    for col in new_hf:
-        for i in range(len(new_hf[col])):
-            new_hf[col][i] = math.degrees(new_hf[col][i])
+            new_hf[col] = half[' UR'] - (m * (half['# LPF']) + c)
 
 
-    print(new_hf)
-    new_hf.to_excel(f'/Users/tomfenemore/Downloads/Clean-{str}.xlsx')
+
+    for i in range(len(new_hf[' UR'])):
+        new_hf[' UR deg'] = math.degrees(new_hf[' UR'][i])
+
+
+
+    #print(new_hf)
+    new_hf.to_excel(f'Clean-{str}.xlsx')
--- main_FE.py
+++ main_FE.py
@@ -3,20 +3,28 @@ import Forcing as f
 import Force_Discretisation as FD
 import Beam as d
 import subprocess
+import Clean as cl
 
 
 #  Define the buckle location and extent
-X = [500, 2]  # Location, Extent
+#X = [500, 2]  # Location, Extent
 
-X = [500, 2]  # This section of code runs the simple analytical calculation
-dml, f_ini, twist = d.beam(X)
-F = FD.smear(f_ini)
-np.savetxt('F.csv', F, delimiter=',')
+#X = [500, 2]  # This section of code runs the simple analytical calculation
+#dml, f_ini, twist = d.beam(X)
+#F = FD.smear(f_ini)
+#np.savetxt('F.csv', F, delimiter=',')
 print('run FE script')
 #sp = subprocess.run('cd', shell=True, capture_output=True)
-sp = subprocess.run('abaqus cae noGUI="FEScript.py"', shell=True,capture_output=True)
-print(sp)
+#sp = subprocess.run('abaqus cae noGUI="FEScript.py"', shell=True,capture_output=True)
+#print(sp)
 print('FE script has finished')
+
+# Now clean the data out of the FE file output
+for i in range(100, 1001, 100):
+    cl.clean(i)
+
+
+
 
 
 
